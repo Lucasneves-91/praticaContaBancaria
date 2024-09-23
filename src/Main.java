ddf4f2cf8149ffe44c82a15e94ee9e8d0208c98d
Lucas Neves <lucas.ccdr@gmail.com>
2024-09-23 15:26:06 -0300

    infos included

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,19 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
+
+        String s = """
+                *****************************
+                Nome: Lucas Santos das Neves
+                Tipo da conta: Conta corrente
+                Saldo Inicial: 
+                ******************************""";
+
+        System.out.println(s);
+
+
+
 
 
     }
 
-    }
+}
