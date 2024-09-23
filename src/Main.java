52260e15518f0a3757763c0124da4555f0542787
Lucas Neves <lucas.ccdr@gmail.com>
2024-09-23 16:09:11 -0300

    menu included

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,47 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
 
-        String s = """
-                *****************************
-                Nome: Lucas Santos das Neves
-                Tipo da conta: Conta corrente
-                Saldo Inicial: 
-                ******************************""";
+        double saldoInicial = 250;
+        double deposito;
+        double transferir;
+        int opcao = 0;
 
-        System.out.println(s);
+        // montar o menu
+        String m = """
+                Qual operação deseja realizar?
+                1 - Consultar saldo
+                2 - Depositar valor
+                3 - Transferir valor
+                4 - Sair
+                """;
+
+        System.out.println(m);
+        Scanner leitor = new Scanner(System.in);
+        opcao = leitor.nextInt();
 
 
+        while (opcao !=4)
+            System.out.println(m);
+            if (opcao == 1) {
+                System.out.println("O seu saldo é de de: R$ " + saldoInicial);
+        } else if (opcao == 2) {
+                System.out.println("Qual valor deseja depositar: R$ ");
+                deposito = leitor.nextDouble();
+                saldoInicial += deposito;
+                System.out.println("Saldo atualizado R$ " + saldoInicial);
+            } else if (opcao == 3) {
+                System.out.println("Qual valor deseja transferir: R$ ");
+                transferir = leitor.nextDouble();
+                saldoInicial -= transferir;
+                System.out.println("Saldo atualizado R$ " + saldoInicial);
+                if (transferir > saldoInicial) {
+                    System.out.println("Você não possui saldo suficiente!");
+                }
 
+
+            }
 
 
     }
